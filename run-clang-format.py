5db0fa1101e08bdfd44047985c2e3ff413d38c07
arashfasihi <arashfasihi@yahoo.com>
2022-05-10 15:28:40 +0200

    Update run-clang-format.py

--- run-clang-format.py
+++ run-clang-format.py
@@ -31,7 +31,7 @@ except ImportError:
     DEVNULL = open(os.devnull, "wb")
 
 
-DEFAULT_EXTENSIONS = 'c,h,C,H,cpp,hpp,cc,hh,c++,h++,cxx,hxx'
+DEFAULT_EXTENSIONS = 'c,h,C,H,cpp,hpp,cc,hh,c++,h++,cxx,hxx,cs'
 DEFAULT_CLANG_FORMAT_IGNORE = '.clang-format-ignore'
 
 
